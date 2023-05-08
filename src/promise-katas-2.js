b4550ce1dd0fd57763ee13fbb1b0e32ad26037e2
AmyHMSmith <amyhmsmith@gmail.com>
2023-05-08 16:21:59 +0100

    promise katas for code review

--- src/promise-katas-2.js
+++ src/promise-katas-2.js
@@ -25,17 +25,48 @@ fetch("joke", "question").then(<insert your callback function>)
 // 1 Create a function that uses the fetch function to make a request to the "food" URL and returns
 // the data - expected return value "Cheese" of type String
 
-const food = () => {};
+// THIS does work but uses the .then
+/* const food = async () => {
+  return await fetch("food")
+    .then(
+      value => { return value.data; }
+    );
+}; */
+
+//THIS works but does not use the .then
+const food = async () => {
+   const { data } = await fetch("food");
+   return data;
+ };
+
 
 // 2 Create a function that uses the fetch function to make a request to the "cats" URL and returns
 // a list of cats in alphabetical order - expected return value ["Bandit", "Berry", "Puss in boots", "Smokey"] of type Array
 
-const cat = () => {};
+const cat = async () => {
+  const promise = await fetch("cats");
+  console.log(promise.data);
+
+   return promise.data.cats.sort();
+  };
+  /* const cat = async () => {
+    const { data } = await fetch("cats");
+    console.log(data);
+
+   return data.cats.sort();
+  }; */
 
 // 3 Create a function that uses the fetch function to make a request to the "dogs" URL and returns
 // the naughtiest dog - expected return value {name: "Mutley", naughty: 10} of type Object
 
-const dog = () => {};
+const dog = async () => {
+const { data } = await fetch("dogs");
+console.log(data);
+
+const naughtiestDog = data.dogs.reduce( (acc, curr) => acc.naughty > curr.naughty ? acc : curr);
+  console.log("Naughty Doggy", naughtiestDog);
+  return(naughtiestDog);
+};
 
 // 4 Create a function that uses the fetch function to make requests to the "jokes" URL and returns
 // a joke object with the key of question and answer - expected return {
@@ -47,7 +78,20 @@ const dog = () => {};
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
 //
 
-const joke = () => {};
+const joke = async () => {
+  try {
+  const { joke } = await fetch("jokes", "question");
+  const { answer } = await fetch("jokes");
+  console.log(joke, answer);
+  return {
+    "question": joke,
+    "answer": answer
+  }
+}
+    catch(data) {
+    console.log(data);
+    }
+  };
 
 module.exports = {
   food,
